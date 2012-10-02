0b7d9595b722ba44cfe67ed864f1b1037b80c4d8
Eugene Ventimiglia <eventi@gmail.com>
2012-10-02 08:03:36 -0400

    Added command line after --

--- restartinator.pl
+++ restartinator.pl
@@ -1,24 +1,45 @@
 #!/usr/bin/env perl
 
-#TODO - Restart on normal exit?
 #TODO - Reporting
+#TODO - Restart on normal exit?
 #TODO - Signal handling
 #TODO - STDOUT, STDERR
+#TODO - exclude from .ignore file
+#TODO - recurse in directory
 
 use File::Monitor;
 use File::Monitor::Object;
 use Proc::Simple;
+use Getopt::Long;
+Getopt::Long::Configure('bundling');
 
-my $command = "/bin/sleep 30";
+my $debug   = 0;
+my $verbose = 0;
+my @files = ( );
 
+my $options = GetOptions(
+	"debug!" =>\$debug,
+	"v|verbose:+" =>\$verbose,
+	"w|watch=s"   =>\@files
+);
+@files = ('.') unless $files[0];
+
+#TODO - get command from remainder of ARGS
+my @cmdargs = @ARGV;
+print STDERR ("ARGV: ",join('|',@ARGV),"\n");
+my $command = join(' ',@cmdargs);
+
+# Proc::Simple::debug($debug);
 my $proc = Proc::Simple->new();
 my $monitor = File::Monitor->new();
 
-#TODO - Get files from arguments, exclude from file
-my @files = ( '.' );
 
+print STDERR ("Watching ",join(":",@files),"\n") if $debug;
 for my $file ( @files ) {
-	$monitor->watch ( $file );
+	$monitor->watch ( {
+			name => $file,
+			recurse => 1
+	});
 }
 
 #Initial Scan
@@ -27,15 +48,21 @@ $monitor->scan;
 my $wannarun = 1;
 my $failed = 0;
 my $running  = 0;
+my $failure_reported = 0;
 
 #Start Process
-$proc->start($command);
-print "Started Command: \"$command\" with pid: ".$proc->pid."\n";
+if ($proc->start(@cmdargs)) {
+	print "Started Command: \"$command\" with pid: ".$proc->pid."\n";
+}else{
+	die "exit_status=".$proc->exit_status."\n";
+}
 
 #After time
 while ($wannarun) {
+	print "Checking\n" if $debug;
 	my $changed = 0;
 	for my $change ( $monitor->scan ) {
+		print "Changed!\n" if $debug;
 		$changed = 1;
 	}
 	$running = $proc->poll();
@@ -43,17 +70,18 @@ while ($wannarun) {
 		print "Change detected!\n";
 		if ($running || $failed){
 			$proc->kill() if $running;
-			$proc->start($command);
+			$proc->start(@cmdargs);
 			print "Started Command: \"$command\" with pid: ".$proc->pid."\n";
+			$failure_reported = 0;
 		}
 	}else{
 		if (! $running){
 			$failed = $proc->exit_status;
 			if ( $failed == 0 ) {
-				print "Normal Exit\n";
 				$wannarun = 0;
 			}else{
-				print "Abnormal exit: $failed\n";
+				print "Abnormal exit: $failed\nWaiting for change before restarting" unless $failure_reported;
+				$failure_reported = 1;
 			}
 		}
 	}
